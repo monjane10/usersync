8f5b4035473a3129a969b98a742c63c41ab36de6
monjane10 <louramonja52.n@gmail.com>
2025-01-20 11:58:10 +0200

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+
+
+
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>Home page content goes here</p>
+        </div>
+    );
+}   
+
+export default Home;
