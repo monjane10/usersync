e7e01c9c9f458ca8493d415467affa9902b751fb
monjane10 <louramonja52.n@gmail.com>
2025-01-20 12:55:24 +0200

    Finalizando o design do frontEnd

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,48 @@
-
+import Trash from '../../assets/Trash.svg'
+import './style.css'
 
 
 function Home() {
+    const users = [{
+        id: 1,
+        name: 'João',
+        age: 20,
+        email: 'joao@gmail.com'
+
+    }, {
+        id: 2,
+        name: 'Antonio',
+        age: 25,
+        email: 'antonio@gmail.com'
+
+    }
+    ]
     return (
-        <div>
-            <h1>Home</h1>
-            <p>Home page content goes here</p>
-        </div>
+        <div className="container">
+            <form action="">
+                <h1>Registo de Utilizadores</h1>
+                <input name="nome" type="text" placeholder='Nome' />
+                <input name="idade" type="text" placeholder='Idade' />
+                <input name="Email" type="email" placeholder='Email'/>
+                <button type="button">Registar</button>
+            </form>
+
+            {users.map(user => (
+                <div key={user.id} className='card'>
+                   <div>
+                   <p>Nome: <span>{user.name}</span> </p>
+                   <p>Idade: <span>{user.age}</span> </p>
+                   <p>Email: <span>{user.email}</span></p>
+               </div>
+               <button>
+                   <img src={Trash} alt="" style={{width: '20px', height: '20px'}} />
+               </button>
+           </div>
+
+    ))
+} 
+ </div >
     );
-}   
+}
 
 export default Home;
